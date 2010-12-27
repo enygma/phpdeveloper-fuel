689322fa8f161873ef1fff12f42a664981c7f6dc
enygma <enygma@phpdeveloper.org>
2010-12-27 10:39:53 -0500

    adding in linking to news_tags table, switching away from static methods

--- fuel/app/classes/model/news.php
+++ fuel/app/classes/model/news.php
@@ -6,13 +6,25 @@ use ActiveRecord;
 
 class News extends ActiveRecord\Model { 
 
+	/*public $has_many = array(
+		array(
+			'news_tags'=>array(
+				'class_name'=>'news_tags',
+				'foreign_key'=>'news_id'
+			)
+		)
+	);*/
+	public $has_many = array(
+		'news_tags'
+	);
+
 	/**
 	* Shortcut to get the latest news details
 	* @return array $results News details
 	*/
-	public static function getLatestNews()
+	public function getLatestNews()
 	{
-		$results = self::find('all',array(
+		$results = $this->find('all',array(
                         'select'	=> array('title','story','date','ID'),
                         'limit'		=> 10,
                         'order'		=> array('date','desc')
@@ -20,6 +32,20 @@ class News extends ActiveRecord\Model {
 		return $results;
 	}
 
+	public function getNewsDetail($newsId)
+	{
+		$results = $this->find($newsId,array('include'=>array('news_tags')));
+		//echo '<pre>'; var_dump($results); echo '<br/>-----------------</br>'; var_dump($results->news_tags); echo '</pre>';
+		foreach($results->news_tags as $tag){
+			echo $tag->tag;
+		}
+		return $results;
+	}
+
+	public function getNewsByTag($tag)
+	{
+		
+	}
 }
 
 /* End of file news.php */
