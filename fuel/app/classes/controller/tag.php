535250bc69980314a784de1077013ac80a721400
enygma <enygma@phpdeveloper.org>
2010-12-27 11:14:01 -0500

    updating to use non-static methods

--- fuel/app/classes/controller/tag.php
+++ fuel/app/classes/controller/tag.php
@@ -10,7 +10,9 @@ class Tag extends Template
 	public function action_index()
 	{
 		$tag = $this->param('tagname');
-		$newsResults = \Fuel\App\Model\News::getNewsByTag($tag);	
+		$News = new \Fuel\App\Model\News;
+
+		$newsResults = $News->getNewsByTag($tag);	
 		$this->render('news/_news-list',array('newsResults'=>$newsResults));
 	}
 
